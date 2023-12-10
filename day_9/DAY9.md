9f3c40e05e11cba78bbaf793136a772a44190086
Ricardo Sisnett <sysnet@revolutionsolid.com>
2023-12-10 20:31:41 +0300

    Part one done! - OH YEAH B-)

---
+++ day_9/DAY9.md
@@ -0,0 +1,10 @@
+# Day 9
+
+## Part One
+
+It read much more complex than it actually was - you just follow the algorithm they suggest to get the sequences and then loop through your new found sequences.
+
+Around 40 lines of code made it work - which I was suspicious because the numbers in the input.txt seemed gigantic and I was expecting that to blow up.
+
+But no - it worked!! :D... Now I'm expecting Part two to be awful (or maybe they're taking it easy cause day 8 was kinda hard?)
+
---
+++ day_9/day_9.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs'
+
+function seq2seq(seq: number[]) : number[]
+{
+    const newSeq = [];
+
+    for (let i = 0; i < seq.length - 1; i++) {
+        newSeq.push(seq[i+1] - seq[i]);
+    }
+
+    console.log(newSeq);
+    return newSeq;
+}
+
+
+function herstory(og: number[]) : number[][] {
+    let seqHistory = [og];
+
+    let curSeq = og;
+    while (curSeq.filter((x) => x == 0).length != curSeq.length)
+    {
+        curSeq = seq2seq(curSeq);
+        seqHistory.push(curSeq);
+    }
+    console.log(seqHistory);
+    seqHistory.reverse();
+
+    for (let i = 0; i < seqHistory.length - 1; i++) {
+        let n = seqHistory[i+1];
+        let c = seqHistory[i];
+        n.push(c[c.length-1] + n[n.length-1]);
+    }
+
+    return seqHistory;
+}
+
+
+/*
+const lines = [
+"0 3 6 9 12 15",
+"1 3 6 10 15 21",
+"10 13 16 21 30 45"]
+*/
+
+
+const lines = fs.readFileSync('input_day_9.txt', 'utf-8').split('\n');
+lines.pop();
+
+let x = lines.map((l) => {
+    const s = l.split(' ').map(c => parseInt(c));
+    return herstory(s).pop()!.pop()!;
+}).reduce((pV, cV) => { return pV+cV }, 0);
+console.log(x);
