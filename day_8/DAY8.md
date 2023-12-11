ec576b26300dbe04c894aace847e47ecc321f102
Ricardo Sisnett <sysnet@revolutionsolid.com>
2023-12-11 08:24:07 +0300

    Part Two - first approach didnt work

--- day_8/DAY8.md
+++ day_8/DAY8.md
@@ -6,3 +6,11 @@ Once you have the hash map, you follow the instructions and index on whatever th
 
 Only devilish detail is to remember that the instructions repeat if needed, so toss in a `%` operator in there.
 
+
+## Part Two
+
+I guess you can just do it in "parallel" (so keep a list of all the current locations, and keep going forward untill all of them end in Z?)
+
+I'm sure there's a trick I'm not seeing - ah, of course... it will just take too long.
+
+Plan 2 - Some sort of 'flooding' or 'dynamic programming' algorithm, basically find the number of steps from every node to a final node and then find the least common multiple among them... or something mathy like that.
--- day_8/day_8.ts
+++ day_8/day_8.ts
@@ -1,30 +1,60 @@
 import * as fs from 'fs'
 
-function createMap(lines: string[]) : Record<string, [string, string]> {
+// Part Two: Return the map and also all starting locations
+function createMap(lines: string[]) : [Record<string, [string, string]>, string[]]  {
     const r: Record<string, [string, string]> = {};
+    const allStarts: string[] = [];
 
     lines.forEach((l) => {
         const [key, destinations] = l.split('=');
         const [left, right] = destinations.split(',');
 
-        r[key.trim()] = [left.trim().slice(1,), right.trim().slice(0, 3)]
+        let k = key.trim();
+
+        r[k] = [left.trim().slice(1,), right.trim().slice(0, 3)]
+
+        if (k[2] === 'A') {
+            allStarts.push(k);
+        }
     });
 
-    return r;
+    return [r, allStarts];
 }
 
 function findWay(allLines: string[]) : number {
     const insts = allLines[0];
     let inst_inx = 0;
-    const map = createMap(allLines.slice(2));
-
-    let s = "AAA";
-
+    let [map, allStarts] = createMap(allLines.slice(2));
     let steps = 0;
+
+    /* Part One
+    let s = "AAA";
     while (s != "ZZZ") {
         steps++;
         let i =  insts[inst_inx] === 'L' ? 0 : 1;
         s = map[s][i];
+        inst_inx = (inst_inx + 1) % insts.length;
+    }
+    */
+    const totalStarts = allStarts.length;
+    let arrived = 0;
+    console.log(totalStarts);
+    while (arrived != totalStarts) {
+        steps++;
+        let i =  insts[inst_inx] === 'L' ? 0 : 1;
+        arrived = 0;
+
+        allStarts = allStarts.map(s => {
+            s = map[s][i];
+
+            if (s[2] == 'Z') { arrived++; }
+
+            return s;    
+        });
+
+        if (arrived > 0)
+            console.log(">> ", arrived); 
+
         inst_inx = (inst_inx + 1) % insts.length;
     }
 
@@ -53,6 +83,20 @@ const lines: string[] =
 "EEE = (EEE, EEE)",
 "GGG = (GGG, GGG)",
 "ZZZ = (ZZZ, ZZZ;)"]
+*/
+/*
+const lines = [
+"LR",
+"",
+"11A = (11B, XXX)",
+"11B = (XXX, 11Z)",
+"11Z = (11B, XXX)",
+"22A = (22B, XXX)",
+"22B = (22C, 22C)",
+"22C = (22Z, 22Z)",
+"22Z = (22B, 22B)",
+"XXX = (XXX, XXX)"
+]
 */
 
 const lines = fs.readFileSync('input_day_8.txt', 'utf-8').split('\n');
