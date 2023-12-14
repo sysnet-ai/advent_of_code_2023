9936cbb4524b83db9204ca9afb8702c3679121f4
Ricardo Sisnett <sysnet@revolutionsolid.com>
2023-12-14 22:52:40 +0300

    Part Two Right before the end of the day :D

--- day_14/DAY14.md
+++ day_14/DAY14.md
@@ -10,3 +10,20 @@ total load.
 
 ## Part Two
 ... Ok, 1000000000 - This needs some mathing around :/
+
+ALRIGHT! We got it.
+
+So first, some code for the cycles - I took the transposing code from yesterday and changed it slightly and *boom* rotation :), if you rotate and then move all the rocks 'north', is the same as moving them to the 'west'.
+
+The complexity was - what's up with the 1000000000000 (too many zeroes?).
+I figured at some point it would start repeating - just like with Day 8.
+
+So, add a hashing function that turns the map into a number and keep a record. Once you find a duplicate, you will need some math:
+
+- First, substract the the cycle when you found the repeat from the 1B - let's call that `C`
+- Now, find the length of the repeating loop - call it `L`. Our record keeps track of at which cycle we found each hash value, so this is easy. Substract the current cycle from the one that we're finding in the record.
+- `C%L` is the amount of cycles left from where you are right now.
+- Calc the load after moving forward that amount of cycles.
+
+
+Solving this one made me feel smort :nerd:
--- day_14/day_14.ts
+++ day_14/day_14.ts
@@ -1,5 +1,32 @@
 import * as fs from 'fs'
 
+function hashish(s_in: string) : number {
+  var hash = 0,
+    i, chr;
+  if (s_in.length === 0) return hash;
+  for (i = 0; i < s_in.length; i++) {
+    chr = s_in.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+function rotate(map: string[][]) : string[][] {
+    const t_map = [];
+
+    for (let i = 0; i < map[0].length; i++) {
+        let row = []; 
+        for (let j = 0; j < map.length; j++) {
+            row.unshift(map[j][i]);
+        }
+        t_map.push(row);
+    }
+
+    return t_map;
+
+}
+
 function rollNorth(map: string[][]) {
 
     for (let i = 0; i < map.length; i++) {
@@ -51,13 +78,54 @@ function countLoad(map: string[][]) : number {
 const map= fs.readFileSync('input_day_14.txt', 'utf-8').split('\n');
 map.pop();
 
-const mapd = map.map((l) => l.split(''));
+let mapd = map.map((l) => l.split(''));
 
-rollNorth(mapd);
-console.log(mapd.forEach(l => console.log(l.join(''))));
-console.log(countLoad(mapd));
+function cycle(t: string[][]) {
+    rollNorth(t);
+//    t.forEach(l => console.log(l.join('')))
+//    console.log('\n\n')
 
+    t = rotate(t);
+    rollNorth(t);
+//    t.forEach(l => console.log(l.join('')))
+//    console.log('\n\n')
 
+    t = rotate(t);
+    rollNorth(t);
+//    t.forEach(l => console.log(l.join('')))
+//    console.log('\n\n')
 
+    t = rotate(t);
+    rollNorth(t);
+//    t.forEach(l => console.log(l.join('')))
+//    console.log('\n\n')
 
+    t = rotate(t);
+//    t.forEach(l => console.log(l.join('')))
+//    console.log('\n\n')
+    return t;
+}
 
+const r:Record<number, number> = {}
+let s = 0;
+let cyclesleft = 1000000000;
+while(cyclesleft) {
+    mapd = cycle(mapd);
+    s++;
+    cyclesleft--;
+
+    let hc = hashish(mapd.map(l => l.join('')).join(''));
+    if (!!r[hc]) {
+       console.log(">>", s, r[hc]);
+
+        cyclesleft = ((1000000000-s)%(s-r[hc]));
+
+       //mapd.forEach(l => console.log(l.join('')));
+       continue; 
+    }
+
+    //console.log("\n\n", "After: ", s);
+    //mapd.forEach(l => console.log(l.join('')));
+    r[hc] = s;
+}
+console.log("LOAD: ", countLoad(mapd));
