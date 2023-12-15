4167e1d898c520745a30c49610c63f3c8951dfb2
Ricardo Sisnett <sysnet@revolutionsolid.com>
2023-12-15 14:00:25 +0300

    Day Two - We on fire again! Now back to day 12

--- day_15/DAY15.md
+++ day_15/DAY15.md
@@ -5,3 +5,12 @@ Today part one is super straight forward, just create a function that follows th
 Split the big string using `split(',')` and then a quick reduce at the very end to sum them all.
 
 EZ PZ... I am worried about part two now :/
+
+## Part Two
+It doesn't seem too scary, maybe they're being nice cause it's friday?
+Again follow the instructions and use a hashmap, do a bit of book keeping and should be good?
+The only thing with the book keeping might be some of the ordering, but I think slices can do most of the heavy lifting for me.
+
+Ok - that worked with almost no adjustments from the original idea. Splice (not slice :/ those are so confusing) does most of the difficult part, but a hash table of lists did it.
+
+Happy Friday!
--- day_15/day_15.ts
+++ day_15/day_15.ts
@@ -1,18 +1,71 @@
 import * as fs from 'fs'
 
-function hashish(s: string) : number {
+function hashish(s: string) : [number, string, string] {
     let cv = 0;
-    for (let i = 0; i < s.length; i++) {
+    let i = 0;
+    for (; i < s.length; i++) {
+        if (s[i] == "=" || s[i] == "-") {
+            break;
+        }
         cv += s.charCodeAt(i);
         cv *= 17;
         cv %= 256;
     }
-    return cv;
+    let x: [number, string, string] = [cv, s.slice(0, i), s.slice(i)];
+    //console.log(x);
+    return x;
 }
+
+function processAction(h: number, lens: string, action: string, record: Record<number, [string, number][]> ) {
+
+    if (!record[h]) {
+        record[h] = [];
+    }
+    let opOn = record[h];
+
+    let foundInx = -1;
+    for (let i = 0; i < opOn.length; i++) {
+        if (opOn[i][0] == lens) {
+            foundInx = i;
+            break;
+        }
+    }
+
+    if (action[0] == "=") {
+        if (foundInx >= 0) {
+            opOn[foundInx][1] = parseInt(action[1]);
+        } else {
+            opOn.push([lens, parseInt(action[1])]);
+        }
+    } else if (action[0] == "-") {
+        if (foundInx >= 0) {
+            opOn.splice(foundInx, 1);
+        }
+    }
+
+    record[h] = opOn;
+} 
 
 //const input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
 const input = fs.readFileSync('input_day_15.txt', 'utf-8').split('\n');
 input.pop();
 
-console.log(input[0].split(',').map((s) => hashish(s)).reduce((cv, pv) => { return cv+pv; }, 0));
+//console.log(input[0].split(',').map((s) => hashish(s)).reduce((cv, pv) => { return cv+pv; }, 0));
+//
+const boxes: Record<number, [string, number][]> = {};
+
+for (let x of input[0].split(',')) {
+    //console.log(x);
+    const [h,lens,action] = hashish(x);
+    processAction(h, lens, action, boxes);
+    //console.log(boxes);
+}
+
+let fpow = 0;
+for (let [bx, lenses] of Object.entries(boxes)) {
+    for (let [inx, [a, b]] of lenses.entries()) {
+        fpow += (parseInt(bx)+1)*(inx+1)*b;
+    }
+}
+console.log(fpow);
