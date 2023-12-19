7e8fef7cdced0c6c681bd689ee0a1cdfee8fb0e5
Ricardo Sisnett <sysnet@revolutionsolid.com>
2023-12-19 16:29:06 +0300

    Part two - Starting approach

--- day_19/DAY19.md
+++ day_19/DAY19.md
@@ -4,8 +4,13 @@
 Seems the harder part of part one is to correctly parse and organize the data.
 I ended up creating a "Functor" to make it easier to keep everything under control and take advantage of some of the data format (i.e. the true paths always return a string for example)
 
-While looking into this, I figured part two is gonna have some large numbers and require some 'collapsing' of the conditions (i.e. "X is accepted IFF <X or >Y") but I figured I'd go for part one before thinking too much about this.
+While looking into this, I figured part two is gonna have some large numbers and require some 'collapsing' of the conditions (i.e. "X is accepted IFF `<X` or `>Y`") but I figured I'd go for part one before thinking too much about this.
 
 
 ## Part Two
 Aaaaaand there it is... haha, I'm starting to get good at preemting the Part two's haha.
+
+Plan:
+BFS and keep track of the "splits" that happen in each node (i.e. `>X` or `<Y`), and when an "A" is found "count" the numbers valid in each split and multiply them.
+
+It does require changing like... all the code thou :/
--- day_19/day_19.ts
+++ day_19/day_19.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs'
 
+// Part One
 class Functor {
     func_str: string;
     if_true: string;
@@ -42,9 +43,22 @@ class Spec {
         this.s = s;
     }
 }
+// </PART ONE>
 
+type Link = Nodetor | null;
+class Nodetor {
+    content: string;
+    if_true: Link;
+    if_false: Link;
+    constructor(content: string, if_true: Link, if_false: Link) {
+        this.content = content;
+        this.if_true = if_true;
+        this.if_false = if_false;
+    }
+}
 
 function qualityAssure(pieceSpecs: Spec, wflows: Record<string, Functor>) : boolean {
+
 
     const {x,m,a,s} = pieceSpecs;
 
@@ -68,6 +82,33 @@ function qualityAssure(pieceSpecs: Spec, wflows: Record<string, Functor>) : bool
 
     }
     return false; // unreachable
+}
+
+function parseLineP2(line: string) : [string, Nodetor] {
+    const [key, body, _] = line.split(/[{}]/);
+
+    let prev: Nodetor;
+    let root: Link = null;
+    body.split(",").forEach(p => {
+        const pcs = p.split(":");
+        let cur;
+        cur = new Nodetor(pcs[0], null, null);
+
+        if (pcs.length > 1) {
+            cur.if_true = new Nodetor(pcs[1], null, null);
+        }
+
+        if (prev && typeof prev != "string") {
+            prev.if_false = cur;
+            if (!root) {
+                root = prev;
+            }
+        }
+        prev = cur;
+    })
+
+
+    return [key, root!];
 }
 
 function parseLine(line: string) : [string, Functor] {
@@ -99,7 +140,6 @@ function parseLine(line: string) : [string, Functor] {
 let lines: string[];
 let specs: string[];
 
-/*
 lines = [
 "px{a<2006:qkq,m>2090:A,rfg}",
 "pv{a>1716:R,A}",
@@ -113,6 +153,7 @@ lines = [
 "gd{a>3333:R,R}",
 "hdj{m>838:A,pv}"]
 
+/*
 specs = [
 "{x=787,m=2655,a=1222,s=2876}",
 "{x=1679,m=44,a=2067,s=496}",
@@ -122,6 +163,7 @@ specs = [
 ]
 */
 
+/*
 const allLines = fs.readFileSync('input_day_19.txt', 'utf-8').split('\n');
 lines = [];
 while (true) {
@@ -139,11 +181,26 @@ lines.forEach(l => {
     let f = parseLine(l);
     r[f[0]] = f[1];
 })
+*/
 
-let spec_list = specs.map(l => {
+
+/*let spec_list = specs.map(l => {
     let [x,m,a,s] = [0,0,0,0];
     eval(l);
     return new Spec(x,m,a,s);
 })
 
 console.log(spec_list.filter(s => qualityAssure(s, r)).map(s => s.x+s.m+s.a+s.s).reduce((pv, cv) => { return pv + cv }, 0));
+*/
+
+let rn: Record<string, Nodetor> = {};
+
+lines.forEach(l => {
+    let f = parseLineP2(l);
+    console.log(f[0], f[1]);
+    rn[f[0]] = f[1];
+});
+
+
+
+
